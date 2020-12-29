daf344099549db20a213230ef7c553c9354b09e8
f0cuS <f0cuSmcsza@gmail.com>
2020-12-29 14:11:20 +0200

    Added form reducer [redux-form]

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux'
+import { reducer as formReducer } from 'redux-form'
 
 import ImageReducer from './ImageReducer'
 
 export default combineReducers({
-    images: ImageReducer
+    images: ImageReducer,
+    form: formReducer
 });
