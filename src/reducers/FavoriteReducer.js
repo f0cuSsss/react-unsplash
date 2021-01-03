c74648804d7c6aacf7f89bd8beeed16c4e99c4f9
f0cuS <f0cuSmcsza@gmail.com>
2021-01-03 17:36:24 +0200

    .

--- src/reducers/FavoriteReducer.js
+++ src/reducers/FavoriteReducer.js
@@ -5,7 +5,7 @@ export default (state = [], action) => {
         case ADD_TO_FAVORITE:
             if(state.find(img => img.id === action.payload.id))
             {
-                return state.filter(img => img.id != action.payload.id);
+                return state.filter(img => img.id !== action.payload.id);
             }
             else {
                 return [...state, action.payload]
